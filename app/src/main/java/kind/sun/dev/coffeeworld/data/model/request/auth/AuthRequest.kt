df75d91e90b27893c4439755d9a76365c65327fd
Duong Tien <mr.duongtiendev@gmail.com>
2023-07-20 22:50:59 +0700

    init: Init AuthRequest and AuthResponse class

---
+++ app/src/main/java/kind/sun/dev/coffeeworld/data/model/request/auth/AuthRequest.kt
@@ -0,0 +1,6 @@
+package kind.sun.dev.coffeeworld.data.model.request.auth
+
+data class AuthRequest(
+    val username: String,
+    val email: String
+)
---
+++ app/src/main/java/kind/sun/dev/coffeeworld/data/model/response/auth/AuthResponse.kt
@@ -0,0 +1,6 @@
+package kind.sun.dev.coffeeworld.data.model.response.auth
+
+data class AuthResponse(
+    val success: Boolean,
+    val data: LoginResult
+)
