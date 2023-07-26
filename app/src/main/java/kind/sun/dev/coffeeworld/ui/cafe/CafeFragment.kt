e09edd9436a3d49339bc2045020c5660cf81a82d
Duong Tien <mr.duongtiendev@gmail.com>
2023-07-26 23:01:29 +0700

    feat: Implement CafeFragment to listen for observable data

--- app/src/main/java/kind/sun/dev/coffeeworld/ui/cafe/CafeFragment.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/ui/cafe/CafeFragment.kt
@@ -8,15 +8,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kind.sun.dev.coffeeworld.databinding.FragmentCafeBinding
+import kind.sun.dev.coffeeworld.utils.api.NetworkResult
 import kind.sun.dev.coffeeworld.utils.common.Logger
+import kind.sun.dev.coffeeworld.utils.view.LoadingDialog
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CafeFragment : Fragment() {
-
     private var _binding: FragmentCafeBinding? = null
     private val binding get() = _binding!!
 
     private val cafeViewModel by viewModels<CafeViewModel>()
+    @Inject lateinit var loadingDialog: LoadingDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -27,7 +30,40 @@ class CafeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Logger.error(cafeViewModel::class.java.simpleName)
+        cafeViewModel.getListCafe()
+        setupInternetLiveData()
+        setupListCafeLiveData()
+    }
+
+    private fun setupInternetLiveData() {
+        cafeViewModel.noInternetLiveData.observe(viewLifecycleOwner) { isDisconnected ->
+            if (isDisconnected) {
+                binding.llBody.visibility = View.GONE
+                binding.imgOffline.visibility = View.VISIBLE
+            } else {
+                binding.imgOffline.visibility = View.GONE
+                binding.llBody.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun setupListCafeLiveData() {
+        cafeViewModel.cafeResponseLiveData.observe(viewLifecycleOwner) {
+            when(it) {
+                is NetworkResult.Success -> {
+                    loadingDialog.dismiss()
+                    Logger.info(it.data!!.data.size.toString())
+                    TODO("Implementation adapter")
+                }
+                is NetworkResult.Error -> {
+                    loadingDialog.dismiss()
+                    Logger.error(it.message.toString())
+                }
+                is NetworkResult.Loading -> {
+                    loadingDialog.show(parentFragmentManager, LoadingDialog::class.simpleName)
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
