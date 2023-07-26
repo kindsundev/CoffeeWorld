c272aac1328e4a5c3bbe0eea223fbbc1fed9e6f4
Duong Tien <mr.duongtiendev@gmail.com>
2023-07-26 23:01:54 +0700

    init: Init CafeAdapter

---
+++ app/src/main/java/kind/sun/dev/coffeeworld/ui/cafe/CafeAdapter.kt
@@ -0,0 +1,8 @@
+package kind.sun.dev.coffeeworld.ui.cafe
+
+import kind.sun.dev.coffeeworld.data.model.response.cafe.CafeModel
+
+class CafeAdapter(
+    private val onCafeClicked: (CafeModel) -> Unit
+) {
+}
