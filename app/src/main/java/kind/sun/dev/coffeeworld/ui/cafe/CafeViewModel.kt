be1167bef4b20edd2467d17791d91fc694b5a60d
Duong Tien <mr.duongtiendev@gmail.com>
2023-07-26 22:59:14 +0700

    feat: Implement CafeViewModel

--- app/src/main/java/kind/sun/dev/coffeeworld/ui/cafe/CafeViewModel.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/ui/cafe/CafeViewModel.kt
@@ -1,9 +1,15 @@
 package kind.sun.dev.coffeeworld.ui.cafe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kind.sun.dev.coffeeworld.data.model.response.cafe.ListCafeResponse
 import kind.sun.dev.coffeeworld.data.repository.CafeRepository
+import kind.sun.dev.coffeeworld.utils.api.NetworkResult
 import kind.sun.dev.coffeeworld.utils.network.NetworkHelper
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +17,18 @@ class CafeViewModel @Inject constructor(
     private val cafeRepository: CafeRepository,
     private val networkHelper: NetworkHelper
 ): ViewModel() {
+    var noInternetLiveData = MutableLiveData<Boolean>(false)
+    val cafeResponseLiveData: LiveData<NetworkResult<ListCafeResponse>>
+        get() = cafeRepository.cafeResponseLiveData
 
+    fun getListCafe() {
+        viewModelScope.launch {
+            noInternetLiveData.value = false
+            if (networkHelper.isConnected) {
+                cafeRepository.getListCafe()
+            } else {
+                noInternetLiveData.value = true
+            }
+        }
+    }
 }
