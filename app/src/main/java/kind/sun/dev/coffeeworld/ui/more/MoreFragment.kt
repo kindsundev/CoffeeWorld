d61e89f3b914e4616532980aff0c3393941ef991
Duong Tien <mr.duongtiendev@gmail.com>
2023-07-30 17:33:22 +0700

    feat: Configure navigation actions and animations for MoreFragment

--- app/src/main/java/kind/sun/dev/coffeeworld/ui/more/MoreFragment.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/ui/more/MoreFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kind.sun.dev.coffeeworld.R
 import kind.sun.dev.coffeeworld.databinding.FragmentMoreBinding
 
 @AndroidEntryPoint
@@ -23,6 +25,57 @@ class MoreFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUtilityListeners()
+        initSupportListeners()
+        initProfileListeners()
+    }
+
+    private fun initUtilityListeners() {
+        binding.utility.apply {
+            explore.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_exploreFragment)
+            }
+            huntForDeals.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_huntDealsFragment)
+            }
+            termsAndConditions.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_termsConditionsFragment)
+            }
+        }
+    }
+
+    private fun initSupportListeners() {
+        binding.support.apply {
+            contactAndFeedback.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_feedbackFragment)
+            }
+            orderReview.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_orderReviewsFragment)
+            }
+        }
+    }
+
+    private fun initProfileListeners() {
+        binding.account.apply {
+            info.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_profileFragment)
+            }
+            security.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_securityFragment)
+            }
+            settings.setOnClickListener {
+                loadFragment(R.id.action_moreFragment_to_settingsFragment)
+            }
+            logout.setOnClickListener { onClickLogout() }
+        }
+    }
+
+    private fun loadFragment(id: Int) {
+        findNavController().navigate(id)
+    }
+
+    private fun onClickLogout() {
+
     }
 
     override fun onDestroyView() {
