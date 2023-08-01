43d689ab6f7bf35ca21e465413bd4eb899c01dce
Duong Tien <mr.duongtiendev@gmail.com>
2023-08-01 23:15:43 +0700

    feat: Implement gallery opening click event handling

--- app/src/main/java/kind/sun/dev/coffeeworld/ui/more/user/profile/AvatarFragment.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/ui/more/user/profile/AvatarFragment.kt
@@ -1,18 +1,50 @@
 package kind.sun.dev.coffeeworld.ui.more.user.profile
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kind.sun.dev.coffeeworld.databinding.FragmentAvatarBinding
+import kind.sun.dev.coffeeworld.utils.common.Logger
 
 @AndroidEntryPoint
-class AvatarFragment: BottomSheetDialogFragment() {
+class AvatarFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentAvatarBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                openImagePicker()
+            } else {
+                Toast.makeText(
+                    requireContext(), "Please provide \'Photo and videos permission\'",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == FragmentActivity.RESULT_OK && result.data != null) {
+                val selectedImageUri = result.data?.data
+                if (selectedImageUri != null) {
+                    val imagePath = selectedImageUri.toString()
+                    Logger.info(imagePath)
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,7 +61,30 @@ class AvatarFragment: BottomSheetDialogFragment() {
     }
 
     fun onClickOpenGallery() {
-        Toast.makeText(requireContext(), "Gallery is open", Toast.LENGTH_SHORT).show()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(requireContext(),
+                    Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openImagePicker()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openImagePicker()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+    private fun openImagePicker() {
+        val pickImageIntentPicker = Intent().apply {
+            action = Intent.ACTION_PICK
+            data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+        pickImageLauncher.launch(pickImageIntentPicker)
     }
 
     fun onClickOpenCamera() {
