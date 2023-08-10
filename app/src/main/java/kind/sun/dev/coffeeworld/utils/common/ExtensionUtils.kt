383874212ebb244186e8f0da018d591d98c322d2
Duong Tien <mr.duongtiendev@gmail.com>
2023-08-10 21:41:06 +0700

    init: Custom getParcelableSafe functions for Parcelable data handling

--- app/src/main/java/kind/sun/dev/coffeeworld/utils/common/ExtensionUtils.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/utils/common/ExtensionUtils.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.core.content.ContextCompat
 import java.io.Serializable
 
@@ -16,6 +17,16 @@ inline fun <reified T: Serializable> Bundle.getSerializableSafe(key: String): T?
 inline fun <reified T : Serializable> Intent.getSerializableSafe(key: String): T? = when {
     Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
     else -> @Suppress("DEPRECATION")getSerializableExtra(key) as T?
+}
+
+inline fun <reified T: Parcelable> Bundle.getParcelableSafe(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
+    else -> @Suppress("DEPRECATION")getParcelable(key) as T?
+}
+
+inline fun <reified T: Parcelable> Intent.getParcelableSafe(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION")getParcelableExtra(key) as T?
 }
 
 inline fun checkSDKTiramisu(onSDKTiramisu: () -> Unit, onNotTiramisu: () -> Unit) {
