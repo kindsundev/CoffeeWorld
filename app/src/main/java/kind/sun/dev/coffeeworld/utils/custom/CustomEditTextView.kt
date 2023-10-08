9852a1cb6b7212fc25fc23904dafceeb10fba4a6
kindsundev <tiendd@eupgroup.net>
2023-10-08 19:47:47 +0700

    refactor: Update custom edit text view with two way data binding

--- app/src/main/java/kind/sun/dev/coffeeworld/utils/custom/CustomEditTextView.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/utils/custom/CustomEditTextView.kt
@@ -2,63 +2,69 @@ package kind.sun.dev.coffeeworld.utils.custom
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.cardview.widget.CardView
+import android.widget.EditText
+import android.widget.FrameLayout
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
+import androidx.databinding.adapters.ListenerUtil
+import androidx.lifecycle.MutableLiveData
 import kind.sun.dev.coffeeworld.R
 import kind.sun.dev.coffeeworld.databinding.CustomEditTextViewBinding
 import kind.sun.dev.coffeeworld.utils.helper.view.dpToPx
-import kind.sun.dev.coffeeworld.utils.helper.view.toEditable
 
 class CustomEditTextView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : CardView(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr){
 
     private val binding by lazy {
         CustomEditTextViewBinding.inflate(LayoutInflater.from(context), this, true)
     }
+
     private var textHint: String? = null
-    private var textValue: String? = null
-    private var icon: Drawable? = null
-    private var background: Drawable? = null
+    private var iconCustom: Drawable? = null
+    private var backgroundCustom: Drawable? = null
     private var editTextType: String? = null
-    private var cardViewRadius: Float? = null
-    private var cardViewElevation: Int? = null
+    private var editText: EditText = binding.editText
+
+    private val hidePasswordDrawable by lazy { getDrawableResource(R.drawable.ic_baseline_visibility_off_24) }
+    private val showPasswordDrawable by lazy { getDrawableResource(R.drawable.ic_baseline_visibility_24) }
+    private var isClickHandled = false
+
+    private fun getDrawableResource(resId: Int): Drawable? = AppCompatResources.getDrawable(context, resId)
 
     init {
         context.obtainStyledAttributes(attrs, R.styleable.CustomEditTextView, 0, 0)
             .let {
                 textHint = it.getString(R.styleable.CustomEditTextView_etv_text_hint)
-                textValue = it.getString(R.styleable.CustomEditTextView_etv_text)
-                icon = it.getDrawable(R.styleable.CustomEditTextView_etv_icon_custom_src)
+                iconCustom = it.getDrawable(R.styleable.CustomEditTextView_etv_icon_custom_src)
                 background = it.getDrawable(R.styleable.CustomEditTextView_etv_background_custom_src)
                 editTextType = it.getString(R.styleable.CustomEditTextView_etv_input_type)
-                cardViewRadius = it.getFloat(R.styleable.CustomEditTextView_etv_card_view_corner_radius, -1f)
-                cardViewElevation = it.getInt(R.styleable.CustomEditTextView_etv_card_view_elevation, -1)
                 it.recycle()
             }.also {
-                initCardView()
-                initEditText()
+                setupEditText()
             }
     }
 
-    /*
-    * Set a shadow for the card view,
-    * the value should be equal to the border of the background to make it more beautiful
-    * */
-    private fun initCardView() = binding.cardView.apply {
-        if (cardViewElevation != -1 && cardViewElevation != null) elevation = cardElevation
-        if (cardViewRadius != -1f && cardViewRadius != null)  radius = cardViewRadius as Float
+    private fun setupEditText() = binding.editText.apply {
+        initEditText()
+        handlePasswordAction()
     }
 
-    private fun initEditText() = binding.editText.apply {
+    private fun EditText.initEditText() {
         compoundDrawablePadding = (16.dpToPx(context))
         textHint?.let { hint = it }
-        textValue?.let { text = it.toEditable() }
-        background?.let { background = it }
+        backgroundCustom?.let { background = it }
+        iconCustom?.let { setCompoundDrawablesRelativeWithIntrinsicBounds(it, null, null, null) }
         inputType = when(editTextType) {
             "text" -> InputType.TYPE_CLASS_TEXT
             "phone" -> InputType.TYPE_CLASS_PHONE
@@ -68,9 +74,75 @@ class CustomEditTextView @JvmOverloads constructor(
             "password" -> InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
             else -> InputType.TYPE_CLASS_TEXT
         }
-        icon?.let {
-            setCompoundDrawablesRelativeWithIntrinsicBounds(it, null, null, null)
+    }
+
+    /*
+    * 1. Get Drawable at compoundDrawables[index]:
+    *   => [DRAWABLE_LEFT] = 0, [DRAWABLE_TOP] = 1, [DRAWABLE_RIGHT] = 2, [DRAWABLE_BOTTOM] = 3
+    * 2. The method OnTouchListener is called twice the touch and release events.
+    *   => So, my check below makes it work like OnCLickListener
+    * */
+    private fun EditText.handlePasswordAction() {
+        if (isPasswordType()) {
+            iconCustom?.let { setCompoundDrawablesRelativeWithIntrinsicBounds(it, null, hidePasswordDrawable, null) }
+            setOnTouchListener { view, motionEvent ->
+                if (motionEvent.rawX >= (right - compoundDrawables[2].bounds.width())) {
+                    isClickHandled = !isClickHandled
+                    if (!isClickHandled) {
+                        view.performClick()
+                        changePasswordVisibility()
+                    }
+                }
+                false
+            }
         }
     }
 
+    private fun EditText.isPasswordType(): Boolean {
+        return inputType == InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+    }
+
+    private fun EditText.changePasswordVisibility() {
+        if (transformationMethod is PasswordTransformationMethod) {
+            transformationMethod = null
+            iconCustom?.let { setCompoundDrawablesRelativeWithIntrinsicBounds(it, null, showPasswordDrawable, null) }
+        } else {
+            transformationMethod = PasswordTransformationMethod.getInstance()
+            iconCustom?.let { setCompoundDrawablesRelativeWithIntrinsicBounds(it, null, hidePasswordDrawable, null) }
+        }
+    }
+
+    companion object {
+
+        @BindingAdapter(value = ["app:textValue"])
+        @JvmStatic
+        fun setTextValue(view: CustomEditTextView, textValue: MutableLiveData<String>) {
+            textValue.value?.let {
+                if(view.editText.text.toString() != it) {
+                    view.editText.setText(it)
+                }
+            }
+        }
+
+        @InverseBindingAdapter(attribute = "app:textValue")
+        @JvmStatic
+        fun getTextValue(view: CustomEditTextView): String {
+            return view.editText.text.toString().trim()
+        }
+
+        @BindingAdapter(value = ["app:textValueAttrChanged"])
+        @JvmStatic
+        fun setTextWatcher(view: CustomEditTextView, textAttrChanged: InverseBindingListener?) {
+            val newValue = object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    textAttrChanged?.onChange()
+                }
+                override fun afterTextChanged(p0: Editable?) {}
+            }
+            val oldValue: TextWatcher? = ListenerUtil.trackListener(view.editText, newValue, R.id.edit_text)
+            oldValue?.let { view.editText.removeTextChangedListener(it) }
+            view.editText.addTextChangedListener(newValue)
+        }
+    }
 }
