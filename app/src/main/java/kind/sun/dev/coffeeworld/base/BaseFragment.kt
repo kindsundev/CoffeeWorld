c5a2f833abdacb94cae8f314968ec31c34ba4bf5
kindsundev <tiendd@eupgroup.net>
2023-10-12 21:55:32 +0700

    feat: Init some fun to BaseFragment

--- app/src/main/java/kind/sun/dev/coffeeworld/base/BaseFragment.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/base/BaseFragment.kt
@@ -1,30 +1,58 @@
 package kind.sun.dev.coffeeworld.base
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
+import kind.sun.dev.coffeeworld.utils.api.NetworkResult
+import kind.sun.dev.coffeeworld.utils.custom.CustomLoadingDialog
+import javax.inject.Inject
 
-abstract class BaseFragment<VDB : ViewDataBinding>(
+abstract class BaseFragment<VDB : ViewDataBinding, VM: ViewModel>(
     private val bindingInflater: (inflater: LayoutInflater) -> VDB
 ) : Fragment() {
 
     private var _binding: VDB? = null
-
     protected val binding: VDB get() = _binding as VDB
+
+    protected abstract val viewModel: VM
+    @Inject lateinit var loadingDialog: CustomLoadingDialog
+
+    private var doubleBackPressed = false
+
+    protected val onDoubleBackExit = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (doubleBackPressed) {
+                requireActivity().finish()
+            } else {
+                doubleBackPressed = true
+                Toast.makeText(context, "Press back again to exit", Toast.LENGTH_SHORT).show()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    doubleBackPressed = false
+                }, 2000)
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = bindingInflater.invoke(inflater)
         setupDataBinding()
-        initialize()
+        prepareData()
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAnything()
         initViews()
         observeViewModel()
     }
@@ -36,12 +64,40 @@ abstract class BaseFragment<VDB : ViewDataBinding>(
 
     abstract fun setupDataBinding()
 
-    abstract fun initialize()
+    abstract fun prepareData()
+
+    open fun initAnything() {}
 
     abstract fun initViews()
 
     abstract fun observeViewModel()
 
+    protected fun <T> handleNetworkResult(
+        liveData: LiveData<NetworkResult<T>>,
+        onSuccess: (T) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        liveData.observe(viewLifecycleOwner) { result ->
+            when(result) {
+                is NetworkResult.Success -> {
+                    if (loadingDialog.isAdded) loadingDialog.dismiss()
+                    result.data?.let { onSuccess.invoke(it) }
+                }
+                is NetworkResult.Error -> {
+                    if (loadingDialog.isAdded) loadingDialog.dismiss()
+                    result.message?.let { onError.invoke(it) }
+                }
+                is NetworkResult.Loading -> {
+                    loadingDialog.show(childFragmentManager, this.tag)
+                }
+            }
+        }
+    }
 
+    protected fun navigateFragment(resId: Int, bundle: Bundle? = null) {
+        findNavController().apply {
+            bundle?.let { navigate(resId, it) } ?: navigate(resId)
+        }
+    }
 
 }
--- app/src/main/java/kind/sun/dev/coffeeworld/view/fragment/home/HomeFragment.kt
+++ app/src/main/java/kind/sun/dev/coffeeworld/view/fragment/home/HomeFragment.kt
@@ -1,18 +1,29 @@
 package kind.sun.dev.coffeeworld.view.fragment.home
 
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kind.sun.dev.coffeeworld.base.BaseFragment
 import kind.sun.dev.coffeeworld.databinding.FragmentHomeBinding
+import kind.sun.dev.coffeeworld.viewmodel.CafeViewModel
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+class HomeFragment : BaseFragment<FragmentHomeBinding, CafeViewModel>(
+    FragmentHomeBinding::inflate
+) {
+
+    override val viewModel: CafeViewModel by viewModels()
+
 
     override fun setupDataBinding() {
 
     }
 
-    override fun initialize() {
+    override fun prepareData() {
+        viewModel.getCafeList()
+    }
 
+    override fun initAnything() {
+        requireActivity().onBackPressedDispatcher.addCallback(onDoubleBackExit)
     }
 
     override fun initViews() {
@@ -20,6 +31,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     }
 
     override fun observeViewModel() {
+        handleNetworkResult(viewModel.cafe,
+            onSuccess = {
 
+            }, onError = {
+
+            })
     }
+
+
 }
